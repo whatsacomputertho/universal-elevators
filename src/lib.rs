08a5b509800afe8ca684ce62ac4c2a8ffa24b8ab
whatsacomputertho <ebalcik71@gmail.com>
2023-12-02 17:13:44 -0500

    Pass string to JS

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
 use wasm_bindgen::prelude::*;
+use std::ffi::CString;
+use std::os::raw::c_char;
+
+static GAME_NAME: &'static str = "Universal Elevators";
 
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-  return a + b;
+pub fn get_game_name() -> *mut c_char {
+  let s = CString::new(GAME_NAME).unwrap();
+  s.into_raw()
+}
+
+#[wasm_bindgen]
+pub fn get_game_name_len() -> usize {
+  GAME_NAME.len()
 }
